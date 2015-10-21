bec852e42641cf8e15d67f3ca6c2fcde63e8c5ad
jbarzas <jbarzas@yahoo.com>
2015-10-21 11:51:55 -0500

    math library
    commit

--- Math Library/Math Library/Math Library/Math_Header.h
+++ Math Library/Math Library/Math Library/Math_Header.h
@@ -48,7 +48,6 @@ Vector3D<T>::Vector3D(T a, T b, T c)
 template <typename T>
 Vector2D<T> add2D(Vector2D<T> a, Vector2D<T> b, Vector2D<T> c)
 {
-	
 	c.x = a.x + b.x;
 	c.y = a.y + b.y;
 
@@ -59,7 +58,6 @@ Vector2D<T> add2D(Vector2D<T> a, Vector2D<T> b, Vector2D<T> c)
 template <typename T>
 Vector3D<T> add3D(Vector3D<T> a, Vector3D<T> b, Vector3D<T> c)
 {
-
 	c.x = a.x + b.x;
 	c.y = a.y + b.y;
 	c.z = a.z + b.z;
@@ -71,7 +69,6 @@ Vector3D<T> add3D(Vector3D<T> a, Vector3D<T> b, Vector3D<T> c)
 template <typename T>
 Vector2D<T> sub2D(Vector2D<T> a, Vector2D<T> b, Vector2D<T> c)
 {
-
 	c.x = a.x - b.x;
 	c.y = a.y - b.y;
 
@@ -82,7 +79,6 @@ Vector2D<T> sub2D(Vector2D<T> a, Vector2D<T> b, Vector2D<T> c)
 template <typename T>
 Vector3D<T> sub3D(Vector3D<T> a, Vector3D<T> b, Vector3D<T> c)
 {
-
 	c.x = a.x - b.x;
 	c.y = a.y - b.y;
 	c.z = a.z - b.z;
@@ -106,6 +102,17 @@ T mag3D(Vector3D<T> a)
 	T mag = sqrt((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
 
 	return mag;
+}
+
+// normalise a 2D Vector
+template <typename T>
+Vector2D<T> norm2D(Vector2D<T> a, Vector2D<T> b, T mag)
+{	
+	Vector2D<T> temp({ a.x / mag, a.y / mag });
+
+	b = temp;
+	
+	return b;
 }
 
 #endif
--- Math Library/Math Library/Math Library/Math_Main.cpp
+++ Math Library/Math Library/Math Library/Math_Main.cpp
@@ -4,59 +4,65 @@ void main()
 {
 	
 	// addition of 2D and 3D Vectors.
-	Vector2D<int> vector2D1(6, 12);
-	Vector2D<int> vector2D2(3, 1);
+	Vector2D<float> vector2D1(6, 12);
+	Vector2D<float> vector2D2(3, 1);
 
-	Vector2D<int> sum2D(0, 0);
+	Vector2D<float> sum2D(0, 0);
 	sum2D = add2D(vector2D1, vector2D2, sum2D);
 
 	cout << "Addition of two 2D Vectors: " << sum2D.x << ", "<< sum2D.y << endl;
 
-	Vector3D<int> vector3D1(6, 12, 4);
-	Vector3D<int> vector3D2(3, 1, 20);
+	Vector3D<float> vector3D1(6, 12, 4);
+	Vector3D<float> vector3D2(3, 1, 20);
 
-	Vector3D<int> sum3D(0, 0, 0);
+	Vector3D<float> sum3D(0, 0, 0);
 	sum3D = add3D(vector3D1, vector3D2, sum3D);
 
 	cout << "Addition of two 3D Vectors: " << sum3D.x << ", " << sum3D.y << ", " << sum3D.z << endl;
 	cout << "\n";
 
 	// subtraction of 2D and 3D Vectors.
-	Vector2D<int> vector2D3(6, 12);
-	Vector2D<int> vector2D4(3, 1);
+	Vector2D<float> vector2D3(6, 12);
+	Vector2D<float> vector2D4(3, 1);
 
-	Vector2D<int> dif2D(0, 0);
+	Vector2D<float> dif2D(0, 0);
 	dif2D = sub2D(vector2D3, vector2D4, dif2D);
 
 	cout << "Subtraction of two 2D Vectors: " << dif2D.x << ", " << dif2D.y << endl;
 
-	Vector3D<int> vector3D3(6, 12, 4);
-	Vector3D<int> vector3D4(3, 1, 20);
+	Vector3D<float> vector3D3(6, 12, 4);
+	Vector3D<float> vector3D4(3, 1, 20);
 
-	Vector3D<int> dif3D(0, 0, 0);
+	Vector3D<float> dif3D(0, 0, 0);
 	dif3D = sub3D(vector3D3, vector3D4, dif3D);
 
 	cout << "Subtraction of two 3D Vectors: " << dif3D.x << ", " << dif3D.y << ", " << dif3D.z << endl;
 	cout << "\n";
 	
 	// magnitude of 2D and 3D Vectors.
-	Vector2D<int> vector2D5(6, 12);
+	Vector2D<float> vector2D5(6, 12);
 
 	float mag1 = mag2D(vector2D5);
 
 	cout << "Magnitude of a 2D Vector: " << mag1 << endl;
-	
-	Vector3D<int> vector3D5(6, 12, 4);
+
+	Vector3D<float> vector3D5(6, 12, 4);
 
 	float mag2 = mag3D(vector3D5);
 
 	cout << "Magnitude of a 3D Vector: " << mag2 << endl;
 	cout << "\n";
-	
-	// normalizing of 2D and 3D Vectors.
-	Vector2D<int> vector2D6(6, 12);
 
-	cout << "Normalising a 2D Vector: " << vector2D6.x << ", " << vector2D6.y << endl;
+	// normalising 2D and 3D Vectors.
+	Vector2D<float> vector2D6(6, 12);
+	Vector2D<float> normal2D(0, 0);
+
+	float mag3 = mag2D(vector2D6);
+
+	norm2D(vector2D6, normal2D, mag3);
+
+	cout << "Normalising a 2D Vector: " << normal2D.x << ", " << normal2D.y << endl;
 	
+
 	system("pause");
 }
